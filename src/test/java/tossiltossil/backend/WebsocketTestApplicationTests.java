344448f843d638e944962c7f073653217e2e6b0d
sangcci <pshsh910@gmail.com>
2025-02-16 18:09:03 +0900

    init: init spring boot projects

---
+++ src/test/java/tossiltossil/backend/WebsocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tossiltossil.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is empty because it is just a test to check if the context loads.
+	}
+}
